51cdf287090be708272a7eab77e9bb18b9964bd5
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2026-03-28 20:48:48 +0530

    Supress hydration warnings

--- packages/claw/src/app/layout.tsx
+++ packages/claw/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <body className="h-full bg-background antialiased">{children}</body>
     </html>
   );
